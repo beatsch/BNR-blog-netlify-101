fe0b6ddfcfbc71944bc0f26e9ebf66e9890f0f10
Bea <36534477+beatsch@users.noreply.github.com>
2022-10-11 14:30:22 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,17 @@
-$(function() {
-		var selectedClass = "";
-		$(".fil-cat").click(function(){ 
-		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300); 
-		
-	});
-});
+ // portfolio
+ $('.gallery ul li a').click(function() {
+     var itemID = $(this).attr('href');
+     $('.gallery ul').addClass('item_open');
+     $(itemID).addClass('item_open');
+     return false;
+ });
+ $('.close').click(function() {
+     $('.port, .gallery ul').removeClass('item_open');
+     return false;
+ });
+
+ $(".gallery ul li a").click(function() {
+     $('html, body').animate({
+         scrollTop: parseInt($("#top").offset().top)
+     }, 400);
+ });
